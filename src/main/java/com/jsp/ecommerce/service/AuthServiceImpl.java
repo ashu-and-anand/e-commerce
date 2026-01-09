42345f9762555187a4fdc2b7d47bc288e62ffdf9
ashu-and-anand <ashutoshanand083.hitece2020@gmail.com>
2026-01-09 16:23:24 +0530

    2nd update ecommerce

--- src/main/java/com/jsp/ecommerce/service/AuthServiceImpl.java
+++ src/main/java/com/jsp/ecommerce/service/AuthServiceImpl.java
@@ -12,7 +12,10 @@ import org.springframework.stereotype.Service;
 
 import com.jsp.ecommerce.dao.UserDao;
 import com.jsp.ecommerce.dto.MerchantDto;
+import com.jsp.ecommerce.dto.OtpDto;
+import com.jsp.ecommerce.entity.Merchant;
 import com.jsp.ecommerce.entity.User;
+import com.jsp.ecommerce.enums.UserRole;
 import com.jsp.ecommerce.security.JwtService;
 import com.jsp.ecommerce.util.EmailService;
 import com.jsp.ecommerce.util.RedisService;
@@ -23,52 +26,89 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
-	private final AuthenticationManager authenticationManager;
-	private final JwtService jwtService;
-	private final UserDetailsService userDetailsService;
-	private final UserDao userDao;
-	private final PasswordEncoder passwordEncoder;
-	private final EmailService emailService;
-	private final RedisService redisService;
+private final AuthenticationManager authenticationManager;
+private final JwtService jwtService;
+private final UserDetailsService userDetailsService;
+private final UserDao userDao;
+private final PasswordEncoder passwordEncoder;
+private final EmailService emailService;
+private final RedisService redisService;
+
+@Override
+public Map<String, Object> login(String email, String password) {
+authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+String token = jwtService.generateToken(userDetails);
+return Map.of("message", "Login Success", "token", token);
+}
+
+@Override
+public Map<String, Object> viewUser(String email) {
+User user = userDao.findByEmail(email);
+return Map.of("message", "Data Found", "user", user);
+}
+
+@Override
+public Map<String, Object> updatePassword(String email, String oldPassword, String newPassword) {
+User user = userDao.findByEmail(email);
+if (passwordEncoder.matches(oldPassword, user.getPassword())) {
+user.setPassword(passwordEncoder.encode(newPassword));
+userDao.save(user);
+return Map.of("message", "Password Updated Success", "user", user);
+}
+throw new IllegalArgumentException("Old Password Not Matching");
+}
+
+@Override
+public Map<String, Object> registerMerchant(MerchantDto merchantDto) {
+if (userDao.checkEmailAndMobieDuplicate(merchantDto.getEmail(), merchantDto.getMobile()))
+throw new IllegalArgumentException("Already Account Exists with Email or Mobile");
+		MerchantDto tempData = redisService.getTempData(merchantDto.getEmail());
+		if (tempData != null)
+			throw new IllegalArgumentException("Already Otp Sent First Verify It or After 30 minutes Try Again");
+Integer otp = generateOtp();
+emailService.sendOtpEmail(otp, merchantDto.getName(), merchantDto.getEmail());
+redisService.saveOtp(otp, merchantDto.getEmail());
+redisService.saveTempData(merchantDto, merchantDto.getEmail());
+		return Map.of("message","Otp Sent Succes Verify within 5 minutes");
+		return Map.of("message", "Otp Sent Succes Verify within 5 minutes");
+}
+
+private Integer generateOtp() {
+return new SecureRandom().nextInt(100000, 1000000);
+}
 
 	@Override
-	public Map<String, Object> login(String email, String password) {
-		authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
-		UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-		String token = jwtService.generateToken(userDetails);
-		return Map.of("message", "Login Success", "token", token);
-	}
-
-	@Override
-	public Map<String, Object> viewUser(String email) {
-		User user = userDao.findByEmail(email);
-		return Map.of("message", "Data Found", "user", user);
-	}
-
-	@Override
-	public Map<String, Object> updatePassword(String email, String oldPassword, String newPassword) {
-		User user = userDao.findByEmail(email);
-		if (passwordEncoder.matches(oldPassword, user.getPassword())) {
-			user.setPassword(passwordEncoder.encode(newPassword));
+	public Map<String, Object> verifyOtp(OtpDto dto) {
+		Integer storedOtp = redisService.getOtp(dto.getEmail());
+		MerchantDto merchantDto = redisService.getTempData(dto.getEmail());
+		if (merchantDto == null)
+			throw new IllegalArgumentException("No Account Exists recreate account");
+		if (storedOtp == null)
+			throw new IllegalArgumentException("Otp Expired, Try Resending");
+		if (storedOtp.equals(dto.getOtp())) {
+			User user = new User(null, merchantDto.getName(), merchantDto.getEmail(), merchantDto.getMobile(),
+					passwordEncoder.encode(merchantDto.getPassword()), UserRole.MERCHANT, true);
 			userDao.save(user);
-			return Map.of("message", "Password Updated Success", "user", user);
+			Merchant merchant = new Merchant(null, merchantDto.getName(), merchantDto.getAddress(),
+					merchantDto.getGstNo(), user);
+			userDao.save(merchant);
+			return Map.of("message", "Account Created Success", "user", merchant);
+		} else {
+			throw new IllegalArgumentException("Otp Missmatch Try Again");
 		}
-		throw new IllegalArgumentException("Old Password Not Matching");
+
 	}
 
 	@Override
-	public Map<String, Object> registerMerchant(MerchantDto merchantDto) {
-		if (userDao.checkEmailAndMobieDuplicate(merchantDto.getEmail(), merchantDto.getMobile()))
-			throw new IllegalArgumentException("Already Account Exists with Email or Mobile");
-		Integer otp = generateOtp();
+	public Map<String, Object> resendOtp(String email) {
+		MerchantDto merchantDto = redisService.getTempData(email);
+		if (merchantDto == null)
+			throw new IllegalArgumentException("No Account Exists recreate account");
+		int otp = generateOtp();
 		emailService.sendOtpEmail(otp, merchantDto.getName(), merchantDto.getEmail());
 		redisService.saveOtp(otp, merchantDto.getEmail());
-		redisService.saveTempData(merchantDto, merchantDto.getEmail());
-		return Map.of("message","Otp Sent Succes Verify within 5 minutes");
-	}
-
-	private Integer generateOtp() {
-		return new SecureRandom().nextInt(100000, 1000000);
+		return Map.of("message", "OTP Resent Success");
 	}
 
 }
--- src/main/java/com/jsp/ecommerce/util/RedisService.java
+++ src/main/java/com/jsp/ecommerce/util/RedisService.java
@@ -13,14 +13,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RedisService {
 
-	private final RedisTemplate<String, Object> redisTemplate;
+private final RedisTemplate<String, Object> redisTemplate;
 
-	public void saveOtp(Integer otp, String email) {
-		redisTemplate.opsForValue().set(email+"_otp", otp,Duration.ofMinutes(5));
+public void saveOtp(Integer otp, String email) {
+redisTemplate.opsForValue().set(email+"_otp", otp,Duration.ofMinutes(5));
+}
+
+public void saveTempData(MerchantDto merchantDto, String email) {
+redisTemplate.opsForValue().set(email+"_merchant", merchantDto,Duration.ofMinutes(30));
+}
+	
+	public Integer getOtp(String email) {
+		return (Integer) redisTemplate.opsForValue().get(email+"_otp");
 	}
-
-	public void saveTempData(MerchantDto merchantDto, String email) {
-		redisTemplate.opsForValue().set(email+"_merchant", merchantDto,Duration.ofMinutes(30));
+	
+	public MerchantDto getTempData(String email) {
+		return (MerchantDto) redisTemplate.opsForValue().get(email+"_merchant");
 	}
 
 }
